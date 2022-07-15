dfbbdd3b4a4841592548783dd2fa68cab71ae4ac
kim_dongwook <kim_dongwook@coconev.co.jp>
2022-07-15 11:17:31 +0900

    init

---
+++ src/views/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>1</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Index extends Vue {}
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/user/login/login_main.vue
@@ -0,0 +1,5 @@
+<template src="./login_temp.html"></template>
+
+<script lang="ts" src="./login_type.ts"></script>
+
+<style lang="scss" src="src/views/user/login/login_style.scss"></style>
